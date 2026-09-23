c1eb4ce1b68524d50c8d13e4d29a78042f648d9f
Dima I <79413560+DIvkov575@users.noreply.github.com>
2026-09-23 09:08:36 -0400

    Solve matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -4,6 +4,12 @@ def matrix_transpose(A: list) -> np.ndarray:
     """
     Returns the transposed matrix as a NumPy array.
     """
-    # a = np.ndarray([np.asarray(x) for x in A])
-    a = np.asarray(A)
-    return a.T
+    A = np.asarray(A)
+    n, m = A.shape
+    T = np.zeros((m, n), dtype=A.dtype)
+    for i in range(n):
+        for j in range(m):
+            T[j, i] = A[i, j]
+    return T
+
+        
